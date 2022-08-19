7ce4f41b4c9647dff95343344e2a504aebf79b7f
Marcel Greter <marcel.greter@ocbnet.ch>
2022-08-19 05:51:12 +0200

    Initial working version

---
+++ Harmony/LightEnablerDuhPOI.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+public class OcbPrettyGrass : IModApi
+{
+
+    public void InitMod(Mod mod)
+    {
+        Debug.Log("Loading OCB POI Light Enabler Patch Duh: " + GetType().ToString());
+        new Harmony(GetType().ToString()).PatchAll(mod.MainAssembly);
+    }
+
+    [HarmonyPatch(typeof(BlockLight))]
+    [HarmonyPatch("GetBlockActivationCommands")]
+    public class BlockLight_GetBlockActivationCommands
+    {
+        static bool Prefix(
+            WorldBase _world,
+            BlockActivationCommand[] ___cmds,
+            ref BlockActivationCommand[] __result)
+        {
+            // Use regular code for editor
+            if (_world.IsEditor()) return true;
+            ___cmds[0].enabled = true;
+            __result = ___cmds;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(BlockLight))]
+    [HarmonyPatch("GetActivationText")]
+    public class BlockLight_GetActivationText
+    {
+        static bool Prefix(
+            WorldBase _world,
+            BlockValue _blockValue,
+            EntityAlive _entityFocusing,
+            BlockActivationCommand[] ___cmds,
+            ref string __result)
+        {
+            // Use regular code for editor
+            if (_world.IsEditor()) return true;
+            PlayerActionsLocal playerInput = ((EntityPlayerLocal)_entityFocusing).playerInput;
+            string str = playerInput.Activate.GetBindingXuiMarkupString() + playerInput.PermanentActions.Activate.GetBindingXuiMarkupString();
+            __result = (_blockValue.meta & 2) != 0 ? string.Format(Localization.Get("useSwitchLightOff"), str) : string.Format(Localization.Get("useSwitchLightOn"), str);
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(BlockLight))]
+    [HarmonyPatch("OnBlockActivated")]
+    public class BlockLight_OnBlockActivated
+    {
+        static MethodInfo FnUpdateLightState = AccessTools.Method(typeof(BlockLight), "updateLightState");
+
+        static bool Prefix(
+            BlockLight __instance,
+            int _indexInBlockActivationCommands,
+            WorldBase _world,
+            int _cIdx,
+            Vector3i _blockPos,
+            BlockValue _blockValue,
+            ref bool __result)
+        {
+            if (_indexInBlockActivationCommands == 0)
+            {
+                FnUpdateLightState.Invoke(__instance, new object[] { _world, _cIdx, _blockPos, _blockValue, true, false });
+                __result = true;
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# OCB Light Enabler POI Mod - 7 Days to Die (A20) Addon
+
+A silly mod to allow anyone to toggle any poi light ¯\\_(ツ)_/¯
+
+Not sure if multiplayer is compatible, but EAC needs to be OFF!
+
+[![GitHub CI Compile Status][4]][3]
+
+## Download and Install
+
+Simply [download here from GitHub][2] and put into your A20 Mods folder:
+
+- https://github.com/OCB7D2D/OcbLightEnablerPOI/archive/master.zip (master branch)
+
+## Changelog
+
+### Version 0.1.0
+
+- Initial version
+
+## Compatibility
+
+I've developed and tested this Mod against version a20.6(b9).
+
+[1]: https://github.com/OCB7D2D/OcbLightEnablerPOI
+[2]: https://github.com/OCB7D2D/OcbLightEnablerPOI/releases
+[3]: https://github.com/OCB7D2D/OcbLightEnablerPOI/actions/workflows/ci.yml
+[4]: https://github.com/OCB7D2D/OcbLightEnablerPOI/actions/workflows/ci.yml/badge.svg
